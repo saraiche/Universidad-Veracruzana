7e537d8041721815aa229a711452f2601c3f990f
JosepHyv <josephynimoto@gmail.com>
2021-05-01 13:56:11 -0500

    fixed

--- Calculadora-Conjuntos/install.sh
+++ Calculadora-Conjuntos/install.sh
@@ -7,7 +7,7 @@ fi
 #instala los requerimientos con pip
 pip3 install -r requeriments.txt
 # Instalacion de la calculadora, y creacion del alias.
-pip3 install --user . && echo "alias conjuntos=/home/$USER/.local/bin/ucl" >> ~/.bash_aliases
+pip3 install --user . && echo "alias conjuntos=/home/$USER/.local/bin/conjuntos" >> ~/.bash_aliases
 #actualizando la terminal
 sudo source ~/.bash_aliases
 #limpiando
